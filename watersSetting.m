81f31b8a9c4ee525b7bcd9c81db95402bf302552
antadlp <ant.adlp1986@gmail.com>
2017-03-24 18:01:40 -0600

    First it will be created a matlab script for a single molecule, in which this molecule serves as templete for the entire thermal bath. The script will have features as the capacity for rotated angle positions.

--- watersSetting.m
+++ watersSetting.m
@@ -18,6 +18,9 @@ fprintf(file, '%d', numA);
 fprintf(file, '\n\tmolec');
 fprintf(file, '\n');
 
+file2 = fopen('settingGeometry.dat','w');
+
+
 for i=1:xd
    for j=1:yd
       for k=1:zd
@@ -38,11 +41,43 @@ for i=1:xd
          fprintf(file, 'H  %f  %f  %f\n', H1(1), H1(2), H1(3));
          fprintf(file, 'H  %f  %f  %f\n', H2(1), H2(2), H2(3));
 
+        
+         %Oxygen is bound only with two hidrogens
+         
+         fprintf(file2, '%d   %d   %d', 2, ...
+         (3*(i)*(j)*(k)-3) + 2, ...
+         (3*(i)*(j)*(k)-3) + 3);
+         fprintf(file2, '\n');
+ 
+
+
+         %Hidrogens bound  with one oxygen and a rigid union
+         %withe the other hidrogen
+         % first %d : number of molecules to bound
+         % second %d : oxigen to bound
+         % third %d : hidrogen to bound 
+
+         fprintf(file2, '%d   %d   %d', 2, ...
+         (3*(i)*(j)*(k)-3) + 1, ...
+         (3*(i)*(j)*(k)-3) + 3);
+         fprintf(file2, '\n');
+ 
+ 
+
+         fprintf(file2, '%d   %d   %d', 2, ...
+         (3*(i)*(j)*(k)-3) + 1, ...
+         (3*(i)*(j)*(k)-3) + 2);
+         fprintf(file2, '\n');
+
+
+
+
       end
    end
 end
 
 fclose(file);
+fclose(file2);
 
 
 
